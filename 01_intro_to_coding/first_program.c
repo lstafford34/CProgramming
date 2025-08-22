049284d126cdce8e6b85e219ce373ee5a8346359
lstafford34 <lstafford3402@floridapoly.edu>
2025-08-22 14:36:00 +0000

    Created first program

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,24 @@
+/*
+
+Multi-line comment.
+08/20/2025
+Lucas Stafford
+The first program in C.
+
+*/
+
+// One line comment.
+
+// Preprocessor directive
+// st.dioh - standard input output
+// .h - header file
+#include <stdio.h>
+
+// Create the main function
+int main(void) {
+    printf("Hello\nWorld\n"); // \n (new line) - escape sequence
+
+    printf("I\nam\nready\nto\ncode!\n");
+
+    return 0;
+}
