c0cb60df4c7278fd4a49bcac7a0636ec20edfaaf
lstafford34 <lstafford3402@floridapoly.edu>
2025-08-22 19:06:59 +0000

    Added student's info

---
+++ 02_student_info/student_info.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+    printf("Name:\tJohn Doe");
+    printf("Course ID:\t2271")
+    printf("Student ID:\t10234")
+    printf("Student ID - Course ID is:\t7963")
+
+    return 0;
+}
