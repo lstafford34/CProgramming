c929eb87226e16035d444a19f276da68a75d7a27
lstafford34 <lstafford3402@floridapoly.edu>
2025-08-25 14:39:11 +0000

    git push

---
+++ 01P_student_info/student_info.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("\nName: \t\t\t\tJohn Doe");
+
+    printf("\nCourse ID: \t\t\t2271");
+
+    printf("\nStudent ID: \t\t\t10234");
+
+    printf("\nStudent ID - Course ID is: \t7963");
+    
+    printf("\n");
+
+    return 0;
+}
---
+++ 02_variables/variables.c
@@ -0,0 +1,26 @@
+/*
+    Introduction to variables in C.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    // Create a variable.
+    int first_number;
+
+    // Assign a value to the variable.
+    first_number = 5;
+
+    // Create and initialize.
+    int second_number = 78;
+
+    // Print the value of a variable.
+    printf("The first number is %d\n", first_number); // %d - conversion specifier
+    printf("The second number is %d\n", second_number);
+
+    // Calculate the sum.
+    int sum = first_number + second_number;
+    printf("%d + %d = %d\n", first_number, second_number, sum);
+
+    return 0;
+}
