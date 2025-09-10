336406ae4104a32d3ce91aac54a115f06e482201
lstafford34 <lstafford3402@floridapoly.edu>
2025-09-10 13:24:01 +0000

    Modified program

---
+++ 08_roots/roots.c
@@ -0,0 +1,64 @@
+/*
+    The program calculates the roots of quadratic equation.
+
+    ax^2 + bx + c = 0
+
+    Steps:
+        1. Print the prompt
+        2. Get coefficients
+        3. Calculate the discriminant
+        4. Print roots
+*/
+
+#include <stdio.h>
+#include <math.h> // sqrt - function
+
+// Function prototypes
+void printPrompt(char coeff);
+int getCoefficient(void);
+int calculateDiscriminant(int a, int b, int c);
+void printRoots(int a, int b, int c);
+int main(void) {
+    // Getting inputs
+    printPrompt('a');
+    int a = getCoefficient();
+    printPrompt('b');
+    int b = getCoefficient();
+    printPrompt('c');
+    int c = getCoefficient();
+    
+    printRoots(a, b, c);
+    
+    return 0;
+}
+
+void printPrompt(char coeff) {
+    printf("Enter the coefficient %c: ", coeff);
+}
+
+int getCoefficient(void) {
+    int coeff;
+    scanf("%d", &coeff);
+
+    return coeff;
+}
+
+int calculateDiscriminant(int a, int b, int c) {
+    return (b * b) - (4 * a * c);
+}
+
+void printRoots(int a, int b, int c) {
+    int discrim = calculateDiscriminant(a, b, c);
+    if (discrim < 0) {
+        puts("No real roots");
+    }
+    else if (discrim == 0) {
+        double x = -b / (2 * a);
+        printf("x = %f\n", x);
+    }
+    else if (discrim > 0) {
+        double x1 = (-b + sqrt(discrim)) / (2 * a); // sqrt - square root
+        double x2 = (-b - sqrt(discrim)) / (2 * a);
+        printf("x1 = %lf; x2 = %lf\n", x1, x2);
+    }
+}
